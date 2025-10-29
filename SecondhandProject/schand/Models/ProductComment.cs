eb8948ee63736a4bf3d113835469f20d7b2c23ea
Dio10072003 <Dohhqe170246@fpt.edu.vn>
2025-10-29 23:40:03 +0700

    feat(backend): add product review and comment models

---
+++ SecondhandProject/schand/Models/ProductComment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace schand.Models;
+
+public partial class ProductComment
+{
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public int? ParentId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual ICollection<ProductComment> InverseParent { get; set; } = new List<ProductComment>();
+
+    public virtual ProductComment? Parent { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ SecondhandProject/schand/Models/ProductReview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace schand.Models;
+
+public partial class ProductReview
+{
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int? Rating { get; set; }
+
+    public string? ReviewContent { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool? IsVerifiedPurchase { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
